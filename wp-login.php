9c3d7aa064ecaf01dae5fe7e25f275b523b458be
Manuel Cebral-Loureda <32591227+mancebral@users.noreply.github.com>
2023-04-07 13:32:44 -0600

    Add files via upload

---
+++ wp-login.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<html lang="gl-ES">
+	<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<title>Iniciar sesión &lsaquo; Cousateca &#8212; WordPress</title>
+	    <style type="text/css">#login h1 a, .login h1 a {
+            background-image: url(./wp-content/themes/cousateca/imx/logo-cousateca-login.png);
+		height:173px;
+		width:150px;
+		background-size: 150px 173px;
+		background-repeat: no-repeat;
+        	padding-bottom: 30px;
+        }
+        #login #nav{display:none}
+        p.submit{text-align:left}
+        .wp-core-ui .button-primary{float:left !important; background:#ff3000 !important; border:none !important; box-shadow:none !important; border-radius:0 !important; text-shadow:none !important}
+        body{background:white !important}
+        .login #login_error, .login .message{border-left:none !important; background:#ededed !important; border:none !important; box-shadow:none !important}
+        .login form{padding:0 !important; box-shadow: none !important}
+        .login label{color:black !important}</style>
+<meta name="robots" content="max-image-preview:large, noindex, noarchive">
+<link rel="stylesheet" id="dashicons-css" href="./wp-includes/css/dashicons.min.css?ver=6.2" type="text/css" media="all">
+<link rel="stylesheet" id="buttons-css" href="./wp-includes/css/buttons.min.css?ver=6.2" type="text/css" media="all">
+<link rel="stylesheet" id="forms-css" href="./wp-admin/css/forms.min.css?ver=6.2" type="text/css" media="all">
+<link rel="stylesheet" id="l10n-css" href="./wp-admin/css/l10n.min.css?ver=6.2" type="text/css" media="all">
+<link rel="stylesheet" id="login-css" href="./wp-admin/css/login.min.css?ver=6.2" type="text/css" media="all">
+	<meta name="referrer" content="strict-origin-when-cross-origin">
+		<meta name="viewport" content="width=device-width">
+	<link rel="icon" href="./wp-content/uploads/2017/09/cropped-favicon-150x150.png" sizes="32x32">
+<link rel="icon" href="./wp-content/uploads/2017/09/cropped-favicon-300x300.png" sizes="192x192">
+<link rel="apple-touch-icon" href="./wp-content/uploads/2017/09/cropped-favicon-300x300.png">
+<meta name="msapplication-TileImage" content="./wp-content/uploads/2017/09/cropped-favicon-300x300.png">
+	</head>
+	<body class="login no-js login-action-login wp-core-ui  locale-gl-es">
+	<script type="text/javascript">document.body.className = document.body.className.replace('no-js','js');</script>
+		<div id="login">
+		<h1><a>Funciona con WordPress</a></h1>
+	
+		<form name="loginform" id="loginform" action="http://cousateca.info/wp-login.php" method="post">
+			<p>
+				<label for="user_login">Nome de usuario ou dirección de correo electrónico</label>
+				<input type="text" name="log" id="user_login" class="input" value="" size="20" autocapitalize="off" autocomplete="username">
+			</p>
+
+			<div class="user-pass-wrap">
+				<label for="user_pass">Contrasinal</label>
+				<div class="wp-pwd">
+					<input type="password" name="pwd" id="user_pass" class="input password-input" value="" size="20" autocomplete="current-password" spellcheck="false">
+					<button type="button" class="button button-secondary wp-hide-pw hide-if-no-js" data-toggle="0" aria-label="Mostrar contrasinal">
+						<span class="dashicons dashicons-visibility" aria-hidden="true"></span>
+					</button>
+				</div>
+			</div>
+						<p class="forgetmenot"><input name="rememberme" type="checkbox" id="rememberme" value="forever"> <label for="rememberme">Lembrarme</label></p>
+			<p class="submit">
+				<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Iniciar sesión">
+									<input type="hidden" name="redirect_to" value="http://cousateca.info/wp-admin/">
+									<input type="hidden" name="testcookie" value="1">
+			</p>
+		</form>
+
+					<p id="nav">
+				<a href="./?action=lostpassword">Perdiches o teu contrasinal?</a>			</p>
+					<script type="text/javascript">function wp_attempt_focus() {setTimeout( function() {try {d = document.getElementById( "user_login" );d.focus(); d.select();} catch( er ) {}}, 200);}
+wp_attempt_focus();
+if ( typeof wpOnload === 'function' ) { wpOnload() }</script>
+				<p id="backtoblog">
+			<a href="./index.html">&larr; Ir a Cousateca</a>		</p>
+			</div>
+				<div class="language-switcher">
+				<form id="language-switcher" action="" method="get">
+
+					<label for="language-switcher-locales">
+						<span class="dashicons dashicons-translation" aria-hidden="true"></span>
+						<span class="screen-reader-text">
+							Idiomas						</span>
+					</label>
+
+					<select name="wp_lang" id="language-switcher-locales"><option value="en_US" lang="en" data-installed="1">English (United States)</option>
+<option value="gl_ES" lang="gl" selected data-installed="1">Galego</option></select>
+					
+					
+					
+						<input type="submit" class="button" value="Cambiar">
+
+					</form>
+				</div>
+				<script type="text/javascript" src="./wp-includes/js/jquery/jquery.min.js?ver=3.6.3" id="jquery-core-js"></script>
+<script type="text/javascript" src="./wp-includes/js/jquery/jquery-migrate.min.js?ver=3.4.0" id="jquery-migrate-js"></script>
+<script type="text/javascript" id="zxcvbn-async-js-extra">
+/* <![CDATA[ */
+var _zxcvbnSettings = {"src":"http:\/\/cousateca.info\/wp-includes\/js\/zxcvbn.min.js"};
+/* ]]> */
+</script>
+<script type="text/javascript" src="./wp-includes/js/zxcvbn-async.min.js?ver=1.0" id="zxcvbn-async-js"></script>
+<script type="text/javascript" src="./wp-includes/js/dist/vendor/wp-polyfill-inert.min.js?ver=3.1.2" id="wp-polyfill-inert-js"></script>
+<script type="text/javascript" src="./wp-includes/js/dist/vendor/regenerator-runtime.min.js?ver=0.13.11" id="regenerator-runtime-js"></script>
+<script type="text/javascript" src="./wp-includes/js/dist/vendor/wp-polyfill.min.js?ver=3.15.0" id="wp-polyfill-js"></script>
+<script type="text/javascript" src="./wp-includes/js/dist/hooks.min.js?ver=4169d3cf8e8d95a3d6d5" id="wp-hooks-js"></script>
+<script type="text/javascript" src="./wp-includes/js/dist/i18n.min.js?ver=9e794f35a71bb98672ae" id="wp-i18n-js"></script>
+<script type="text/javascript" id="wp-i18n-js-after">
+wp.i18n.setLocaleData( { 'text directionltr': [ 'ltr' ] } );
+</script>
+<script type="text/javascript" id="password-strength-meter-js-extra">
+/* <![CDATA[ */
+var pwsL10n = {"unknown":"Fortaleza da contrasinal descoñecida.","short":"Moi débil","bad":"Débil","good":"Medio","strong":"Forte","mismatch":"Non coinciden"};
+/* ]]> */
+</script>
+<script type="text/javascript" id="password-strength-meter-js-translations">( function( domain, translations ) {
+	var localeData = translations.locale_data[ domain ] || translations.locale_data.messages;
+	localeData[""].domain = domain;
+	wp.i18n.setLocaleData( localeData, domain );
+} )( "default", {"translation-revision-date":"2023-03-29 07:41:04+0000","generator":"GlotPress\/4.0.0-alpha.4","domain":"messages","locale_data":{"messages":{"":{"domain":"messages","plural-forms":"nplurals=2; plural=n != 1;","lang":"gl_ES"},"%1$s is deprecated since version %2$s! Use %3$s instead. Please consider writing more inclusive code.":["¡%1$s está obsoleto desde a versión %2$s! Usa %3$s no seu lugar. Por favor, plantéxate escribir un código máis inclusivo."]}},"comment":{"reference":"wp-admin\/js\/password-strength-meter.js"}} );</script>
+<script type="text/javascript" src="./wp-admin/js/password-strength-meter.min.js?ver=6.2" id="password-strength-meter-js"></script>
+<script type="text/javascript" src="./wp-includes/js/underscore.min.js?ver=1.13.4" id="underscore-js"></script>
+<script type="text/javascript" id="wp-util-js-extra">
+/* <![CDATA[ */
+var _wpUtilSettings = {"ajax":{"url":"\/wp-admin\/admin-ajax.php"}};
+/* ]]> */
+</script>
+<script type="text/javascript" src="./wp-includes/js/wp-util.min.js?ver=6.2" id="wp-util-js"></script>
+<script type="text/javascript" id="user-profile-js-extra">
+/* <![CDATA[ */
+var userProfileL10n = {"user_id":"0","nonce":"af9e3ff27f"};
+/* ]]> */
+</script>
+<script type="text/javascript" id="user-profile-js-translations">( function( domain, translations ) {
+	var localeData = translations.locale_data[ domain ] || translations.locale_data.messages;
+	localeData[""].domain = domain;
+	wp.i18n.setLocaleData( localeData, domain );
+} )( "default", {"translation-revision-date":"2023-03-29 07:41:04+0000","generator":"GlotPress\/4.0.0-alpha.4","domain":"messages","locale_data":{"messages":{"":{"domain":"messages","plural-forms":"nplurals=2; plural=n != 1;","lang":"gl_ES"},"Your new password has not been saved.":["O teu novo contrasinal non foi gardado"],"Hide":["Ocultar"],"Show":["Amosar"],"Confirm use of weak password":["Confirma o uso dun contrasinal débil"],"Hide password":["Ocultar contrasinal"],"Show password":["Mostrar contrasinal"]}},"comment":{"reference":"wp-admin\/js\/user-profile.js"}} );</script>
+<script type="text/javascript" src="./wp-admin/js/user-profile.min.js?ver=6.2" id="user-profile-js"></script>
+	<div class="clear"></div>
+	</body>
+	</html>
